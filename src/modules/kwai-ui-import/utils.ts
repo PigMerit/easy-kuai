9591b80ff84e95f5952d56db377b455aa5a58017
liuzhenpeng <liuzhenpeng@kuaishou.com>
2022-03-09 11:12:55 +0800

    feat():单行引入优化-末尾换行

--- src/modules/kwai-ui-import/utils.ts
+++ src/modules/kwai-ui-import/utils.ts
@@ -177,21 +177,21 @@ export const insertComponentsCode = (document: vscode.TextDocument, item: vscode
             item.additionalTextEdits.push(
                 vscode.TextEdit.insert(
                     new vscode.Position(namePosition[0].lineIdx + 1, 0),
-                    `components: {${insertText}},`,
+                    `components: {${insertText}},\n`,
                 ),
             );
         } else if (decoratorPosition.length > 0) {
             item.additionalTextEdits.push(
                 vscode.TextEdit.insert(
                     new vscode.Position(decoratorPosition[0].lineIdx + 1, 0),
-                    `components: {${insertText}},`,
+                    `components: {${insertText}},\n`,
                 ),
             );
         } else {
             item.additionalTextEdits.push(
                 vscode.TextEdit.insert(
                     new vscode.Position(compositionPosition[0]?.lineIdx || 0 + 1, 0),
-                    `components: {${insertText}}`,
+                    `components: {${insertText}},\n`,
                 ),
             );
         }
